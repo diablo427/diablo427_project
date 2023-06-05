72054fae1c5629f6f5c62bbf19ef7666767d454c
diablo427 <zcx1345478@163.com>
2023-06-05 23:49:26 +0800

    20230601

---
+++ test/test002.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 6/5/23 11:48 PM
+# @Author  : diablo427
+# @Email   :  zcx1345478@163.com
+# @File    : test002.py
+# @Software : PyCharm
+
+
+if __name__ == '__main__':
+    print("hello")
